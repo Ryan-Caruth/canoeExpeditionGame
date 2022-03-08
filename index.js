6ef2fb6fd0a44fc15db7555b3d549f2d821c2f17
Ryan-Caruth <ryan.caruth2@gmail.com>
2022-03-08 08:43:37 -0700

    Added names to array

--- index.js
+++ index.js
@@ -10,10 +10,24 @@ const compName = ["Jack", "Jill", "Fred", "Steve", "John", "Sarah", "Philip",
     "Elvis", "Sidney", "Lorrie", "Carly", "Jake", "Katie", "Nicole", "Evan", "Tim",
     "Tom", "Jaden", "Ashley", "Theodore", "Dory", "Jody", "Judy", "Peggy",
     "Wally", "Wallace", "Jessie", "Tony", "Ethan", "Janette", "Gord",
-    "Jenifer", "Brandon", "Betty", "Mary", "Skyler", "Tucker", "Chelsea"]
+    "Jenifer", "Brandon", "Betty", "Mary", "Skyler", "Tucker", "Chelsea",
+    "Ally", "Aidan", "Andrew", "Bailey", "Billy", "Bob", "Catherine",
+    "Hayley", "Annabelle", "Kyle", "Ryder", "Marcus", "Kiera", "Zoe", "Rose",
+    "Elena", "Elliot", "Nicolas", "Julian", "Jordan", "Annie", "Frank",
+    "Mason", "Benson", "Mark", "Blake", "Tessa", "Gabriella", "Dawson",
+    "Christopher", "Harvey", "Lucas", "Angela", "Ellie", "Aspen",
+    "Caroline", "Zane", "Xander", "Tanner", "Sophie", "Adeline",
+    "Brooklyn", "Gabriel", "Sean", "Chase", "Heidi", "Kayla", "Anatasia",
+    "Amy", "Stephanie", "Myles", "Kevin", "Grayson", "Poppy", "Corbin",
+    "Hayden", "Bradley", "Weston", "Westley", "Maeve", "Kylie", "Anthony",
+    "Jax", "Barett", "Hannah", "Stella", "Jessica", "Alexa", "Aaron", 
+    "Derek", "David", "Riley", "Zander", "Lilly", "Ruby", "Penelope", 
+    "Natalie", "Aurora", "Gracie", "Grace", "Maddison", "Walker", "Alicia",
+    "Bella", "Octavia", "Brianna", "Vivian", "Eleanor", "Danica", "Andrea",
+    "Morgan", "Serena", "Alexandra", "Mackenzie", "Addison"]
 
 const numOfNames = compName.length
 
 const randomName = Math.floor(numOfNames * Math.random());
 
-console.log("Your partner for this trip is " + compName[randomName] + ".");
+console.log(`Your canoe partner for this trip is ${compName[randomName]}.`);
