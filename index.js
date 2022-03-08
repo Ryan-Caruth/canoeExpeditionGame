4d4596f0abf1d94c0d62edd7665165ae6617e14d
Ryan-Caruth <ryan.caruth2@gmail.com>
2022-03-07 17:39:19 -0700

    Added a random name generator

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 let readlineSync = require("readline-sync");
 
 // Wait for user's response.
-let name = readlineSync.question("May I have your name? ");
+let name = readlineSync.question("May I have your first name? ");
 console.log("Hello " + name + "." + " Welcome to either your best canoe expirence or your worst canoe expirence.");
 
+const compName = ["Jack", "Jill", "Fred", "Steve", "John", "Sarah", "Philip",
+    "Liana", "Maggie", "Jade", "Jodie", "Hope", "Greg", "Graham", "Cory",
+    "Reid", "Patrick", "Sam", "Samatha", "Tobey", "Brian", "Ryan", "Rachel",
+    "Elvis", "Sidney", "Lorrie", "Carly", "Jake", "Katie", "Nicole", "Evan", "Tim",
+    "Tom", "Jaden", "Ashley", "Theodore", "Dory", "Jody", "Judy", "Peggy",
+    "Wally", "Wallace", "Jessie", "Tony", "Ethan", "Janette", "Gord",
+    "Jenifer", "Brandon", "Betty", "Mary", "Skyler", "Tucker", "Chelsea"]
+
+const numOfNames = compName.length
+
+const randomName = Math.floor(numOfNames * Math.random());
+
+console.log("Your partner for this trip is " + compName[randomName] + ".");
