00ce229f8e93a8632eaa69737fbdc462308914d5
Ryan-Caruth <ryan.caruth2@gmail.com>
2022-03-10 21:57:01 -0700

    Added a API of giveName to declare your name

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const res = require("express/lib/response");
+const { giveName } = require('./index');
 
 const app = express();
 
@@ -12,8 +13,18 @@ const PORT = 5000;
 app.listen(PORT, echoPortNumber);
 
 app.get('/startGame', (req, res) => {
-    res.send( 
-        "Welcome to 'Canadian eh', a game that will test your instincts in a canoe. Please copy this link and paste in your terminal to continue."
+    res.send(
+      "Welcome to 'Canadian eh', a game that will test your instincts in a canoe.\
+    Please copy this link and paste in your terminal to continue curl http://localhost:5000/giveName?name={Enter your name here}."
     );
+});
+
+
+
+app.get('/giveName', (req, res) => {
+    let name = req.query.name;
+    giveName.name = name;
+    res.send(`Hello ${name}`)
+    console.log(name);
 });
 
