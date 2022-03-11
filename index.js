7f224a2a47e0941143da0a13099fc145226a5df5
Ryan-Caruth <ryan.caruth2@gmail.com>
2022-03-11 15:03:17 -0700

    Commented out code

--- index.js
+++ index.js
@@ -11,19 +11,21 @@ const giveName = {
 module.exports = { giveName } 
 
 //Create an array for randomly generated name
-// const compName = ["Jack", "Jill", "Fred", "Steve", "John", "Sarah", "Philip", "Liana", "Maggie", "Jade", "Jodie", "Hope", "Greg",
-// "Graham", "Cory", "Reid", "Patrick", "Sam", "Samatha", "Tobey", "Brian", "Ryan", "Rachel", "Elvis", "Sidney", "Lorrie", "Carly", "Jake",
-// "Katie","Nicole", "Evan", "Tim", "Tom", "Jaden", "Ashley", "Theodore", "Dory", "Jody", "Judy", "Peggy", "Wally", "Wallace", "Jessie", "Tony",
-// "Ethan", "Janette", "Gord", "Jenifer", "Brandon", "Betty", "Mary", "Skyler", "Tucker", "Chelsea", "Ally", "Aidan", "Andrew", "Bailey",
-// "Billy", "Bob", "Catherine", "Hayley", "Annabelle", "Kyle", "Ryder", "Marcus", "Kiera", "Zoe", "Rose", "Elena", "Elliot", "Nicolas",
-// "Julian", "Jordan", "Annie", "Frank", "Mason", "Benson", "Mark", "Blake", "Tessa", "Gabriella", "Dawson", "Christopher", "Harvey",
-// "Lucas", "Angela", "Ellie", "Aspen", "Caroline", "Zane", "Xander", "Tanner", "Sophie", "Adeline", "Brooklyn", "Gabriel", "Sean", "Chase",
-// "Heidi", "Kayla", "Anatasia", "Amy", "Stephanie", "Myles", "Kevin", "Grayson", "Poppy", "Corbin", "Hayden", "Bradley", "Weston", "Westley",
-// "Maeve", "Kylie", "Anthony", "Jax", "Barett", "Hannah", "Stella", "Jessica", "Alexa", "Aaron", "Derek", "David", "Riley", "Zander",
-// "Lilly", "Ruby", "Penelope", "Natalie", "Aurora", "Gracie", "Grace", "Maddison", "Walker", "Alicia", "Bella", "Octavia", "Brianna",
-// "Vivian", "Eleanor", "Danica", "Andrea", "Morgan", "Serena", "Alexandra", "Mackenzie", "Addison"];
+// const compName =
+//   ["Jack", "Jill", "Fred", "Steve", "John", "Sarah", "Philip", "Liana", "Maggie", "Jade", "Jodie", "Hope", "Greg",
+//     "Graham", "Cory", "Reid", "Patrick", "Sam", "Samatha", "Tobey", "Brian", "Ryan", "Rachel", "Elvis", "Sidney", "Lorrie", "Carly", "Jake",
+//     "Katie", "Nicole", "Evan", "Tim", "Tom", "Jaden", "Ashley", "Theodore", "Dory", "Jody", "Judy", "Peggy", "Wally", "Wallace", "Jessie", "Tony",
+//     "Ethan", "Janette", "Gord", "Jenifer", "Brandon", "Betty", "Mary", "Skyler", "Tucker", "Chelsea", "Ally", "Aidan", "Andrew", "Bailey",
+//     "Billy", "Bob", "Catherine", "Hayley", "Annabelle", "Kyle", "Ryder", "Marcus", "Kiera", "Zoe", "Rose", "Elena", "Elliot", "Nicolas",
+//     "Julian", "Jordan", "Annie", "Frank", "Mason", "Benson", "Mark", "Blake", "Tessa", "Gabriella", "Dawson", "Christopher", "Harvey",
+//     "Lucas", "Angela", "Ellie", "Aspen", "Caroline", "Zane", "Xander", "Tanner", "Sophie", "Adeline", "Brooklyn", "Gabriel", "Sean", "Chase",
+//     "Heidi", "Kayla", "Anatasia", "Amy", "Stephanie", "Myles", "Kevin", "Grayson", "Poppy", "Corbin", "Hayden", "Bradley", "Weston", "Westley",
+//     "Maeve", "Kylie", "Anthony", "Jax", "Barett", "Hannah", "Stella", "Jessica", "Alexa", "Aaron", "Derek", "David", "Riley", "Zander",
+//     "Lilly", "Ruby", "Penelope", "Natalie", "Aurora", "Gracie", "Grace", "Maddison", "Walker", "Alicia", "Bella", "Octavia", "Brianna",
+//     "Vivian", "Eleanor", "Danica", "Andrea", "Morgan", "Serena", "Alexandra", "Mackenzie", "Addison"] 
+
 // const numOfNames = compName.length;
-// const randomName = Math.floor(numOfNames * Math.random());
+const randomName = Math.floor(numOfNames * Math.random());
 // console.log(`\nYour canoe partner for this trip is ${compName[randomName]}.`);
 
 //Let the usere choose a canoe
